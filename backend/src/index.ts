322375426b662b604c327c6f2247667ffa29971c
udanjin <hayashidasora@gmail.com>
2025-09-03 02:30:04 +0700

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,8 +8,9 @@ export default app;
 
 // Untuk development
 if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3001;
+  const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    console.log(`🌍 Environment: ${process.env.NODE_ENV}`);
   });
 }
