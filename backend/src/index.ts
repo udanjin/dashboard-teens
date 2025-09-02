b49a47f0deff99258efc4e274701191cfabae7d8
udanjin <hayashidasora@gmail.com>
2025-09-03 02:46:02 +0700

    updat

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,10 @@
-import { Server } from "./server";
+import { Server } from './server';
 
+// Gunakan PORT dari environment variable, atau 4000 jika tidak ada
+const PORT =parseInt(process.env.PORT || '8080', 10);
 const server = new Server();
 const app = server.getApp();
 
-// Untuk Vercel
-export default app;
-
-// Untuk development
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 8080;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    console.log(`🌍 Environment: ${process.env.NODE_ENV}`);
-  });
-}
+app.listen(PORT, '0.0.0.0', () => { // Tambahkan '0.0.0.0'
+  console.log(`🚀 Server running on port ${PORT}`);
+});
