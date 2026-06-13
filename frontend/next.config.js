b8ec858bb54db627b1aa1cb7aa02fa14d73141c4
udanjin <hayashidasora@gmail.com>
2026-06-14 01:15:30 +0700

    updet

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `https://api.atmosphereteens.my.id/api/:path*`,
+        destination: `https://api-atmosphereteens.my.id/api/:path*`,
       },
     ];
   },
